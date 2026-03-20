cbd1df2b02b31facb902c05af56c0e40de7aadc4
Rohan29-code <pattapagalurohansai@gmail.com>
2026-03-20 08:52:22 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> consist = new ArrayList<>();
+
+        System.out.println("Train initialized successfully.");
+        System.out.println("Initial bogie count: " + consist.size());
+    }
+}
